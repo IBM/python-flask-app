0c50d07fab777babdb83e8d5e1098875de81493b
Chuck Cox <chuckc@us.ibm.com>
2021-03-31 12:24:12 -0500

    new: flasgger module

--- server/routes/swagger.py
+++ server/routes/swagger.py
@@ -1,5 +1,21 @@
-from swagger_ui import flask_api_doc
+from flasgger import Swagger
 from server import app
 
 
-flask_api_doc(app, config_path='public/swagger.yaml', url_prefix='/swagger/api-docs', title='API doc')
+swagger_config = {
+    "headers": [
+    ],
+    "specs": [
+        {
+            "endpoint": 'apispec_1',
+            "route": '/apispec_1.json',
+            "rule_filter": lambda rule: True,  # all in
+            "model_filter": lambda tag: True,  # all in
+        }
+    ],
+    "static_url_path": "/flasgger_static",
+    # "static_folder": "static",  # must be set by user
+    "swagger_ui": True,
+    "specs_route": "/swagger/api-docs/"
+}
+Swagger(app, template_file='../public/swagger.yaml', config=swagger_config)
