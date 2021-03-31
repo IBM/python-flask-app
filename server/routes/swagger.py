72201c58e902b0f090cbf200e264da56d2a5783b
Chuck Cox <chuckc@us.ibm.com>
2021-03-31 15:01:02 -0500

    chore: removing swagger (#79)
    * chore: removing swagger
    
    * new: swagger-ui-py
    
    * chore: rm wrong files
    
    * chore: add swagger dependency
    
    * fix: wrong dependency
    
    * chore: rm swagger from Dockerfile
    
    * chore: removed space
    
    * new: flasgger module
    
    * new: flasgger module

--- server/routes/swagger.py
+++ server/routes/swagger.py
@@ -1,13 +1,22 @@
-
-from flask import jsonify, render_template
+from flasgger import Swagger
 from server import app
 
-@app.route("/swagger/api")
-def swagger_api():    
-    with open("public/swagger.yaml", "r") as f:
-        content = f.read()
-    return "<pre>"+content+"</pre>"
 
-@app.route("/explorer")
-def explorer():
-    return render_template('swagger-ui/index.html')
+swagger_config = {
+    "headers": [
+    ],
+    "specs": [
+        {
+            "endpoint": 'apispec_1',
+            "route": '/apispec_1.json',
+            "rule_filter": lambda rule: True,  # all in
+            "model_filter": lambda tag: True,  # all in
+        }
+    ],
+    "static_url_path": "/flasgger_static",
+    # "static_folder": "static",  # must be set by user
+    "swagger_ui": True,
+    "specs_route": "/swagger/api-docs/"
+}
+
+Swagger(app, template_file='../public/swagger.yaml', config=swagger_config)
